39634d2258894b1c4fd1da11fb9039f94db4a30b
Igor Zorin <igor_zorin@mail.ru>
2021-03-21 20:40:02 +0200

    пишет функцию uniq

--- 03-objects-arrays-intro-to-testing/4-uniq/index.js
+++ 03-objects-arrays-intro-to-testing/4-uniq/index.js
@@ -3,6 +3,6 @@
  * @param {*[]} arr - the array of primitive values
  * @returns {*[]} - the new array with uniq values
  */
-export function uniq(arr) {
-
+export function uniq(arr = []) {
+  return [...new Set(arr)];
 }
