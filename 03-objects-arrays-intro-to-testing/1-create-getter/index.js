3af0757bc5d80c381d70427a8ddca61fba41bcdf
Igor Zorin <igor_zorin@mail.ru>
2021-03-21 22:03:17 +0200

    пишет функцию createGetter

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,17 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
+const PATH_SEPARATOR = '.';
 
+export function createGetter(path) {
+  return (obj) => {
+    const pathParts = path.split(PATH_SEPARATOR);
+
+    return pathParts
+      .reduce(
+        (
+          currentValue,
+          currentPathChunk
+        ) => currentValue?.[currentPathChunk], obj);
+  };
 }
